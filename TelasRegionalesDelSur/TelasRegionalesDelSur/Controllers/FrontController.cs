545c611805c9dd4e21b32eacafb38da2359af416
Straiker10 <diosemirnah96@outlook.com>
2018-06-14 22:30:42 -0500

    Vista y controlador Front

---
+++ TelasRegionalesDelSur/TelasRegionalesDelSur/Controllers/FrontController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelasRegionalesDelSur.Controllers
+{
+    public class FrontController : Controller
+    {
+        //
+        // GET: /Front/
+
+        public ActionResult InicioFront()
+        {
+            return View();
+        }
+
+    }
+}
