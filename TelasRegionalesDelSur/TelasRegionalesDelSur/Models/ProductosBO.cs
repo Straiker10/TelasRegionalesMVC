9b13dd834154dc87b302a3ea8ac94014ab3cc2bb
Straiker10 <diosemirnah96@outlook.com>
2018-06-21 09:47:26 -0500

    Clases

---
+++ TelasRegionalesDelSur/TelasRegionalesDelSur/Models/ProductosBO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelasRegionalesDelSur.Models
+{
+    public class ProductosBO
+    {
+        public int ID { get; set; }
+        public string NombreProducto { get; set; }
+        public string Descripcion { get; set; }
+        public string Precio { get; set; }
+        public string imagen { get; set; }
+       
+    }
+}
